57701b28eded9eabf4778669962a69ffd83f8bda
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-11 09:24:21 +0100

    Finish client-side setup

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,7 @@
 // Starting point for client-side application
 
-console.log('hellooo')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'))
