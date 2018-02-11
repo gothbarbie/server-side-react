c60c68b0e6861a17c7ef4df6d01591efb559f868
Hanna Söderström <gothbarbie84@gmail.com>
2018-02-11 09:32:22 +0100

    Refactor webpack configuration

--- webpack.client.js
+++ webpack.client.js
@@ -1,27 +1,14 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {  
+const clientConfig = {
   entry: './src/client/client.js', // Client-side entry-point
-  
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public') // Put in folder "public"
-  },
-
-  module: {
-    rules: [
-      {
-        test:  /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react', 
-            'stage-0', 
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 }
+
+module.exports = merge(baseConfig, clientConfig)
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {
+const serverConfig = {
   target: 'node', // Note! Build for Node (not browser, as per default)
   
   entry: './src/index.js',
@@ -8,22 +10,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
-
-  module: {
-    rules: [
-      {
-        test:  /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react', 
-            'stage-0', 
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 }
+
+module.exports = merge(baseConfig, serverConfig)
